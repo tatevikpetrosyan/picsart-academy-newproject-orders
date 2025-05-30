8184e1091a249320493b91f839fe565d7862f188
tatevikpetrosyan <tatevpetrosyan90@gmail.com>
2025-05-30 23:23:33 +0400

    new project homework

---
+++ src/main/java/org/example/models/Customer.java
@@ -0,0 +1,37 @@
+package org.example.models;
+
+public class Customer {
+    private String name;
+    private String surname;
+    private String mobile;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
