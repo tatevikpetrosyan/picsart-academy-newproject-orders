e228d4cfc8798a8399fe573e31189f8f0b365d60
tatevikpetrosyan <tatevpetrosyan90@gmail.com>
2025-06-13 23:31:08 +0400

    Enum homework

---
+++ src/main/java/org/example/enumHomework/DayOfWeek.java
@@ -0,0 +1,13 @@
+package org.example.enumHomework;
+
+public enum DayOfWeek {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY;
+
+
+}
---
+++ src/main/java/org/example/enumHomework/Season.java
@@ -0,0 +1,17 @@
+package org.example.enumHomework;
+
+public enum Season {
+    WINTER("7"),
+    SPRING("15"),
+    SUMMER("28"),
+    AUTUMN("22");
+
+    private String temp;
+    Season(String temp) {
+        this.temp = temp;
+    }
+
+    public String getAverageTemp() {
+        return this.temp;
+    }
+}
