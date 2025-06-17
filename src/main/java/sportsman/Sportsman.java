082f8dac1c838e33d01c649d48df5ef1bb96032d
tatevikpetrosyan <tatevpetrosyan90@gmail.com>
2025-06-17 22:30:55 +0400

    new changes

--- src/main/java/sportsman/Sportsman.java
+++ src/main/java/sportsman/Sportsman.java
@@ -1,6 +1,8 @@
 package sportsman;
 
 public abstract class Sportsman {
+    public SportCategory sc1 = SportCategory.TEAM;
+    public SportCategory sc2 = SportCategory.SINGLE;
 
     public abstract void play();
 }
