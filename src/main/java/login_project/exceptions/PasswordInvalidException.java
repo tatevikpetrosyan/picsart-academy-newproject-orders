93163f89ee96afcfaf45c1a15f389dd44256a8a9
tatevikpetrosyan <tatevpetrosyan90@gmail.com>
2025-07-12 15:35:07 +0400

    New login project homework, try-catch, UserPosts

---
+++ src/main/java/login_project/exceptions/PasswordInvalidException.java
@@ -0,0 +1,7 @@
+package login_project.exceptions;
+
+public class PasswordInvalidException extends RuntimeException {
+    public PasswordInvalidException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/login_project/exceptions/UserInvalidExceprion.java
@@ -0,0 +1,7 @@
+package login_project.exceptions;
+
+public class UserInvalidExceprion extends Exception {
+    public UserInvalidExceprion(String message) {
+        super(message);
+    }
+}
